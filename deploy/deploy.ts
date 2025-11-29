5cff5f53dafee00f176e2aa9ed9722a58f94ca04
alexmasonx <bybooz76755@outlook.com>
2025-11-29 14:36:39 +0800

    deploy on sepolia

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedMapGame = await deploy("EncryptedMapGame", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`EncryptedMapGame contract: `, deployedMapGame.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_encrypted_map_game"; // id required to prevent reexecution
+func.tags = ["EncryptedMapGame"];
